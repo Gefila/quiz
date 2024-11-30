9b1b555743c7f340b56ec7f5f923b18a81657aa1
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2024-11-30 17:54:42 +0700

    Add progress bar to Info component for quiz navigation

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,8 +1,11 @@
 export default function Info({number, score, quizDataLength}) {
     return(
-        <div className="flex flex-row justify-between items-center gap-2 text-white px-2 ">
+        <>
+        <div className="flex flex-row justify-between items-center gap-2 text-white">
             <div className="">Question {number + 1} / {quizDataLength}</div>
             <div className="">Score: {score}</div>
         </div>
+        <progress className="progress progress-primary w-full" value={number+1} max={quizDataLength}></progress>
+        </>
     )
 }
